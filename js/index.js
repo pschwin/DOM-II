245719382af97f70ce6b39375a7c1278092e86f7
Patrick <patrick.schwindt14@gmail.com>
2019-01-29 14:08:11 -0700

    Another Event

--- js/index.js
+++ js/index.js
@@ -5,11 +5,13 @@
 const header = document.querySelector('header');
 const nav = document.querySelector('nav')
 const body = document.querySelector('body');
-const headings = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
+//const headings = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
 const funnerBus = document.querySelector('header, nav, button');
-const creeperNav = document.querySelector('main-navigation');
+const busImage = document.querySelector('.intro img');
+const intro = document.querySelector('.intro');
 
-console.log(funnerBus);
+
+console.log(busImage);
 
 
 //New Nav appendChild
@@ -30,9 +32,28 @@ funnerBus.addEventListener ('click', e => {
     body.style.fontFamily = "Eater";
     body.style.backgroundColor = "Black";
     body.style.color = "white";
-    headings.style.fontFamily = "Creepster";
-})
+    //headings.style.fontFamily = "Creepster";
 
-//Event Listener 2
+    //Event Listener 2
+
+    busImage.addEventListener ('mouseover', e2 =>{
+
+        busImage.setAttribute('src', 'img/160906-clowns-in-nc-feature.jpg');
+        // let newClown = document.createElement('img');
+        // newClown.setAttribute('src', 'img/160906-clowns-in-nc-feature.jpg');
+        // intro.appendChild(newClown);
+        busImage.style.filter = "grayscale(100%)";
+        busImage.style.borderRadius = "10px";
+    
+    });
+
+    //Event Listener 3
+
+
+});
+
+
+
+
 
 
